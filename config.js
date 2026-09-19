48ce9c99a7f1e865697aafd5e23e214b9b2f0a84
yerang-k <yeahskyr@gmail.com>
2026-09-19 16:41:32 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Apps Script 웹앱 배포 후 나온 URL(…/exec)을 아래 따옴표 안에 붙여 넣으세요.
+// 비워 두면 학생 페이지는 예전처럼 이 기기에만 저장됩니다(교사 페이지는 동작하지 않음).
+const API_URL='https://script.google.com/macros/s/AKfycbyq06AzV1mWEJ1pKYN6DB4E4kkb3j8oZ42GG4Hz2wPEEo-tgYy_2_MY94Kt1mFupCan0Q/exec';
